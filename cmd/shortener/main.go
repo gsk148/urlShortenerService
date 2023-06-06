9f7bea3d5afad7cb6430e4142b4a614ae2e237e8
Anton Shelkov <shelkovaf@mos.ru>
2023-06-06 09:10:41 +0300

    iter1 change conditions

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,58 +7,57 @@ import (
 	"time"
 )
 
-var shortString = ""
 var urlsMap = make(map[string]string)
-var addedUrls = make(map[string]string)
+
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-func addShortOrGetLargeURL(w http.ResponseWriter, r *http.Request) {
-	parsedURL := r.URL
-	if parsedURL.Path != "" && r.Method == http.MethodGet {
-
-		url := parsedURL.Path[1:]
-		if urlsMap[url] == "" {
-			w.WriteHeader(http.StatusBadRequest)
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Reading requestBody failed", http.StatusBadRequest)
 			return
 		}
 
-		w.WriteHeader(http.StatusTemporaryRedirect)
-		w.Write([]byte("Location: " + urlsMap[url]))
-		return
-	}
+		scheme := "http"
+		if r.TLS != nil {
+			scheme = "https"
+		}
 
-	if r.Method != http.MethodPost {
+		if len(body) == 0 {
+			http.Error(w, "Empty request body", http.StatusBadRequest)
+			return
+		}
+		encoded := createRandomStringFromInput(string(body))
+		urlsMap[encoded] = string(body)
+		w.Header().Set("content-type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+		url := scheme + "://" + r.Host + "/" + encoded
+		w.Write([]byte(url))
+	} else if r.Method == http.MethodGet {
+		path := r.URL.Path
+		if len(path) < 2 {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+		path = path[1:]
+		if _, ok := urlsMap[path]; ok {
+			w.Header().Set("Location", urlsMap[path])
+			w.WriteHeader(http.StatusTemporaryRedirect)
+			return
+		} else {
+			http.Error(w, "Short url not found", http.StatusBadRequest)
+			return
+		}
+
+	} else {
 		http.Error(w, "Not supported", http.StatusBadRequest)
 		return
 	}
-
-	requestData, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Reading requestData failed", http.StatusBadRequest)
-		return
-	}
-
-	if len(requestData) == 0 {
-		http.Error(w, "Empty request body", http.StatusBadRequest)
-		return
-	}
-
-	inputString := string(requestData)
-	if addedUrls[inputString] != "" {
-		shortString = addedUrls[inputString]
-	} else {
-		shortString = createRandomStringFromInput(inputString)
-	}
-
-	urlsMap[shortString] = inputString
-	content := "http://localhost:8080/" + shortString
-
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(content))
 }
 
 func createRandomStringFromInput(url string) string {
@@ -67,15 +66,12 @@ func createRandomStringFromInput(url string) string {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
 
-	short := string(b)
-	addedUrls[url] = short
-
-	return short
+	return string(b)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, addShortOrGetLargeURL)
+	mux.HandleFunc(`/`, handler)
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
 		panic(err)
