a499b92bd6fc32248d55eeb9c2189aa560da4204
Anton Shelkov <shelkovaf@mos.ru>
2023-07-21 06:45:38 +0300

    iter11 add using db storage

--- internal/app/storage/database.go
+++ internal/app/storage/database.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	_ "github.com/lib/pq"
 )
@@ -13,6 +14,18 @@ type DBStorage struct {
 
 func NewDBStorage(dsn string) *DBStorage {
 	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec(`
+        CREATE TABLE IF NOT EXISTS shortener (
+            id SERIAL PRIMARY KEY,
+            uuid TEXT NOT NULL,
+            short_url TEXT NOT NULL UNIQUE,
+            original_url TEXT NOT NULL
+        );
+    `)
 	if err != nil {
 		panic(err)
 	}
@@ -30,11 +43,37 @@ func (s *DBStorage) Ping() error {
 }
 
 func (s *DBStorage) Store(data ShortenedData) error {
+	_, err := s.DB.ExecContext(context.Background(),
+		"INSERT INTO shortener (uuid, short_url, original_url) VALUES ($1, $2, $3)",
+		data.UUID, data.ShortURL, data.OriginalURL)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (s *DBStorage) Get(key string) (ShortenedData, error) {
-	return ShortenedData{}, nil
+	var (
+		uuid        string
+		shortURL    string
+		originalURL string
+	)
+
+	row := s.DB.QueryRowContext(context.Background(),
+		"SELECT uuid, short_url, original_url FROM shortener WHERE short_url = $1", key)
+	err := row.Scan(&uuid, &shortURL, &originalURL)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return ShortenedData{}, errors.New("key not found: " + key)
+		} else {
+			return ShortenedData{}, err
+		}
+	}
+	return ShortenedData{
+		UUID:        uuid,
+		ShortURL:    shortURL,
+		OriginalURL: originalURL,
+	}, nil
 }
 
 func (s *DBStorage) Close() error {
